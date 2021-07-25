002886dd3daf690e659c8b64760ee2e1ac4d83c8
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-25 22:46:46 +0100

    introducao a pipes

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -14,9 +14,21 @@ export class GifsService {
   }
 
   //Inserir valores  no principio 
-  buscarGifs( query: string ) {
+  buscarGifs(query: string) {
+
+    query = query.trim().toLocaleLowerCase();
     
-    this._historial.unshift(query);
+    if (query.trim().length === 0 ) {
+      return;
+    }
+    
+
+    if ( !this._historial.includes( query )) {
+          this._historial.unshift(query);
+          this._historial = this._historial.splice(0, 10);
+    }
+
+  
     
     console.log(this._historial);
 
