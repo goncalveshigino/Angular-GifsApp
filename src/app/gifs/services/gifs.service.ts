6f35b2f9bbdcfd109f8b31af9d310c920ba2c741
goncalveshigino <goncalveshigino20@gmail.com>
2021-07-26 03:41:11 +0100

    feito

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -52,7 +52,6 @@ export class GifsService {
     
     this.http.get<SearchGifsResponse>(`${ this.serviceURL }/search`, { params })
       .subscribe((resp) => {
-        console.log(resp.data);
         this.resultados = resp.data;
 
         localStorage.setItem('resultados', JSON.stringify( this.resultados ) );
